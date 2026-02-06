ef6c53f5dfcdc9fb7caa251e42480ec276ee0eeb
Harshit Garg <gargh2120@gmail.com>
2026-02-06 22:34:00 +0530

    All interfaces are defined

--- src/Types/index.d.ts
+++ src/Types/index.d.ts
@@ -1,5 +1,52 @@
- interface ButtonProp {
+import type { ReactNode } from "react";
+
+interface ButtonProp {
   title: string;
   classname?: string;
-  type? : "submit" | "button";
+  type?: "submit" | "button";
 }
+
+interface WrapperProp {
+  children: ReactNode;
+}
+
+interface StoreProp {
+  currency: string;
+  courses : dummyCoursesProp[] | null;
+  FetchAllCourses: () => void;
+}
+interface CourseCardProp {
+  course : dummyCoursesProp
+}
+interface dummyCoursesProp {
+    _id: string;
+    courseTitle: string;
+    courseDescription: string;
+    coursePrice: number;
+    isPublished: boolean;
+    discount: number;
+    courseContent: {
+        chapterId: string;
+        chapterOrder: number;
+        chapterTitle: string;
+        chapterContent: {
+            lectureId: string;
+            lectureTitle: string;
+            lectureDuration: number;
+            lectureUrl: string;
+            isPreviewFree: boolean;
+            lectureOrder: number;
+        }[];
+    }[];
+    educator: string;
+    enrolledStudents: string[];
+    courseRatings: {
+        userId: string;
+        rating: number;
+        _id: string;
+    }[];
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+    courseThumbnail: string;
+}[]
