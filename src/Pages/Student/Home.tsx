e9a7b2f679aeebdc33b66bfa0bb7e7be0313f7a0
Harshit Garg <gargh2120@gmail.com>
2026-02-07 22:46:56 +0530

    adding new links

--- src/Pages/Student/Home.tsx
+++ src/Pages/Student/Home.tsx
@@ -1,19 +1,26 @@
-import Hero from '../../Componets/Student/Hero'
-import SearchBar from '../../Componets/Student/SearchBar'
-import Companies from '../../Componets/Student/Companies'
-import CouseSection from '../../Componets/Student/CouseSection'
+import Hero from "../../Componets/Student/Hero";
+import SearchBar from "../../Componets/Student/SearchBar";
+import Companies from "../../Componets/Student/Companies";
+import CouseSection from "../../Componets/Student/CouseSection";
+import Testimonial from "../../Componets/Student/Testimonial";
+import Footer from "../../Componets/Student/Footer";
+import Wrapper from "../../Componets/ReuseCompo/Wrapper";
+import Login from "./Login";
 
 function Home() {
   return (
-    <div className='mx-auto'>
-    
-      <Hero/>
-      <SearchBar/> 
-      <Companies/>
-      <CouseSection/>
+    <div className="mx-auto">
+      <Wrapper>
+        <Hero />
+        <SearchBar />
+        <Companies />
+        <CouseSection />
+        <Testimonial />
+      </Wrapper>
 
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
