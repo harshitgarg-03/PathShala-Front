d78d400625043112a0436d85786d0ae0e51927fa
Harshit Garg <gargh2120@gmail.com>
2026-02-07 22:57:15 +0530

    adding new links

--- src/Componets/Student/Hero.tsx
+++ src/Componets/Student/Hero.tsx
@@ -1,21 +1,65 @@
-import React from 'react'
-import Heroimg from '../../assets/Hero.png'
-import Button from '../ReuseCompo/Button'
-import "../../index.css"
+import React from "react";
+import Heroimg from "../../Data/Hero.png";
+import Button from "../ReuseCompo/Button";
+import "../../index.css";
 function Hero() {
   return (
-    <div className={`flex px-18 justify-between min-w-screen`}>
-        <div className={`h-128 w-auto flex justify-center gap-2 pl-8 flex-col`}>
-            <h1 className={`text-6xl font-semibold leading-18 mb-4`} >Empower Your Learning Journey</h1>
-            <p className={`text-gray-600`} >Explore high-quality courses and advance your skills </p>
-            <div className={`flex gap-4 mt-10`}>
-                <Button title='Explore Courses' classname='bg-green-500 font-semibold text-lg w-50 font-sans text-white'/>
-                <Button title='Get Started'classname='bg-blue-800 font-semibold w-50 font-sans text-white'/>
-            </div>
-        </div>
-        <img src={Heroimg} alt="Hero" className={`w-auto h-132`} />
+    <div
+      className={`flex flex-col md:flex-row h-auto md:h-120 lg:h-130
+  items-center mx-auto
+  px-3  md:px-2 lg:px-4
+  gap-10 md:gap-0`}
+    >
+      <div
+  className="flex flex-col justify-center gap-6
+  mt-6 sm:mt-8
+  px-4 sm:px-8 md:px-12
+  text-center md:text-left
+  max-w-xl md:max-w-2xl"
+>
+  <div className="mb-5">
+
+    <h1
+      className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl
+      font-bold leading-snug tracking-tight mb-4"
+    >
+      Empower Your Learning Journey
+    </h1>
+
+    <p className="text-gray-600 text-sm sm:text-base leading-relaxed max-w-md">
+      Explore high-quality courses and advance your skills
+    </p>
+
+  </div>
+
+  <div
+    className="flex flex-col sm:flex-row items-center
+    gap-4 mt-3 sm:mt-5
+    justify-center md:justify-start"
+  >
+    <Button
+      title="Explore Courses"
+      classname="bg-green-500 font-semibold text-base sm:text-lg w-40 sm:w-44 md:w-48 font-sans text-white"
+    />
+
+    <Button
+      title="Get Started"
+      classname="bg-blue-800 font-semibold w-40 sm:w-44 md:w-48 font-sans text-white"
+    />
+  </div>
+</div>
+
+      <div className={`flex justify-center w-full md:w-auto`}>
+        <img
+          src={Heroimg}
+          alt="Hero"
+          className={`object-contain
+      h-100 sm:h-80 md:h-104 lg:h-130
+      w-auto`}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
