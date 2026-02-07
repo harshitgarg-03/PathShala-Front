3457c7c27888f43fac585df9bff09f7189a69d99
Harshit Garg <gargh2120@gmail.com>
2026-02-07 22:51:16 +0530

    adding new links

--- src/ZustandStore/Store.tsx
+++ src/ZustandStore/Store.tsx
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import type { StoreProp } from "../Types";
 import { dummyCourses } from "../Data/assets";
+import { useNavigate } from "react-router-dom";
 
 export const useStore = create<StoreProp>((set) => ({
   courses: null,
@@ -9,5 +10,17 @@ export const useStore = create<StoreProp>((set) => ({
     set({
       courses: dummyCourses,
     });
+  },
+
+  // navigate: useNavigate(),
+
+  CourseRatingFunction: (Courses) => {
+    if (Courses.courseRatings.length == 0) return 0;
+
+    let TotalRating = 0;
+
+    Courses.courseRatings.map((user) => (TotalRating += user.rating));
+
+    return (TotalRating / Courses.courseRatings.length);
   },
 }));
