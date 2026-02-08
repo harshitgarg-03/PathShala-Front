cd6bcf92c1d195433377ebbc7900a3de780dd0d6
Harshit Garg <gargh2120@gmail.com>
2026-02-08 23:20:15 +0530

    fix error

--- src/Componets/Student/Loading.tsx
+++ src/Componets/Student/Loading.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useAuth } from '../../ZustandStore/AuthStore'
+import type { LoadingProp } from '../../Types';
 
-function Loading() {
+function Loading(classname : LoadingProp) {
+  const isLoading = useAuth(s => s.isLoading);
+  if(!isLoading) return;
   return (
-    <div>Loading</div>
+    <div className={`fixed inset-0 bg-white/80 backdrop-blur-sm z-50 flex items-center ${classname} justify-center`}>
+
+      <div
+        className="
+        h-12 w-12
+        border-4 border-blue-200
+        border-t-blue-700
+        rounded-full
+        animate-spin
+        "
+      />
+
+    </div>
   )
 }
 
