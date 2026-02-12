f11ad27a29d92d14c7d0f302ebcc9a295366ee45
Harshit Garg <gargh2120@gmail.com>
2026-02-12 23:18:08 +0530

    fix error

--- src/Pages/Educator/Educator.tsx
+++ src/Pages/Educator/Educator.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../../Componets/Educator/NavBar";
+import Wrapper from "../../Componets/ReuseCompo/Wrapper";
+import DashBoard from "./DashBoard";
+import Footer from "../../Componets/Student/Footer";
+import AddCourse from "./AddCourse";
 
 function Educator() {
   return (
     <div>
-      Educator
-      {<Outlet />}
+      <NavBar />
+      <Wrapper>{<Outlet />}</Wrapper>
     </div>
   );
 }
