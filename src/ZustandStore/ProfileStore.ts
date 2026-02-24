86c5589a6ab51d12c37305bd53f59b6fcf9997b0
Harshit Garg <gargh2120@gmail.com>
2026-02-24 21:33:33 +0530

    adding function

--- src/ZustandStore/ProfileStore.ts
+++ src/ZustandStore/ProfileStore.ts
@@ -1,12 +1,39 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import type { ProfileProp } from "../Types";
+import { api } from "../lib/api";
+import { useAuth } from "./AuthStore";
 
 export const UseProfile = create<ProfileProp>()(
-  persist((set) => ({
-    isEducator: false,
+  persist(
+    (set, get) => ({
 
-  }), {
-    name: "Profile-Store",
-  }),
+      isLoading: useAuth.getState().isLoading,
+      error: useAuth.getState().error,
+      isEducator: false,
+      UpdateProfile: async (data) => {
+      //  console.log("daata ", data.getAll("firstname"));
+        console.log("daats", data.get("name"));
+        console.log("daats", data.get("firstname"));
+        
+        set({ isLoading: true });
+        try {
+          const res = await api.post("/UpdateProfile", data, {
+            headers: {
+              "Content-Type": "multipart/form-data"
+            }
+          });
+          useAuth.setState({
+            user: res.data.data
+          })
+          set({ isLoading: false });
+        } catch (error: any) {
+          set({ isLoading: false, error: error.data });
+        }
+      },
+    }),
+    {
+      name: "Profile-Store",
+    },
+  ),
 );
