480fcb671b3e624b54768f754811659bdc61a529
Harshit Garg <gargh2120@gmail.com>
2026-02-25 21:53:46 +0530

    adding function

--- src/ZustandStore/AnalyticsStore.ts
+++ src/ZustandStore/AnalyticsStore.ts
@@ -7,7 +7,7 @@ export const AnalyticsStore = create<AnalyticsStoreProp>((set, get) => ({
     TotalStudents: 0,
     AvgCoursePrice: 0,
     TotalCourse: 0, 
-    AverageRating: 0,
+    AverageRating: 0, 
     TotalRevenue : () => {
         const UserFetchedCourse = CourseStore.getState().UserFetchedCourse;
         let TotalStudentsCourse = 0;
