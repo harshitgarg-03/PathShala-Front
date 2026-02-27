9c7ed09b1da427a0226f44d9ed12c0332817b89f
Harshit Garg <gargh2120@gmail.com>
2026-02-27 12:58:40 +0530

    optimizing the profile store and reduce code lines

--- src/ZustandStore/ProfileStore.ts
+++ src/ZustandStore/ProfileStore.ts
@@ -1,49 +1,59 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 import type { ProfileProp } from "../Types";
 import { api } from "../lib/api";
 import { useAuth } from "./AuthStore";
 
-export const UseProfile = create<ProfileProp>()(
-  persist(
-    (set, get) => ({
+export const UseProfile = create<ProfileProp>((set) => ({
+  isLoading: false,
+  error: null,
+  isEducator: false,
 
-      isLoading: useAuth.getState().isLoading,
-      error: useAuth.getState().error,
+  UpdateProfile: async (data) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const res = await api.post("/UpdateProfile", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      useAuth.setState({
+        user: res.data.data,
+      });
+
+      set({ isLoading: false });
+    } catch (error: any) {
+      set({
+        isLoading: false,
+        error:
+          error.response?.data?.message || "Profile update failed",
+      });
+    }
+  },
+
+  changedpassword: async (data) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      await api.post("/changed-password", data);
+
+      set({ isLoading: false });
+    } catch (error: any) {
+      set({
+        isLoading: false,
+        error:
+          error.response?.data?.message || "Password change failed",
+      });
+    }
+  },
+
+  // on logout 
+  resetProfile: () => {
+    set({
+      isLoading: false,
+      error: null,
       isEducator: false,
-      UpdateProfile: async (data) => {
-      //  console.log("daata ", data.getAll("firstname"));
-        console.log("daats", data.get("name"));
-        console.log("daats", data.get("firstname"));
-        
-        set({ isLoading: true });
-        try {
-          const res = await api.post("/UpdateProfile", data, {
-            headers: {
-              "Content-Type": "multipart/form-data"
-            }
-          });
-          useAuth.setState({
-            user: res.data.data
-          })
-          set({ isLoading: false });
-        } catch (error: any) {
-          set({ isLoading: false, error: error.data });
-        }
-      },  
-      
-      changedpassword: async(data) => {
-        set({ isLoading: true });
-        try {
-          const res = await api.post("/changed-password", data);
-          set({ isLoading: false });
-        } catch (error: any) {
-          set({ isLoading: false, error: error.data });
-        }
-      }
-    }),
-    {
-      name: "Profile-Store",
-    },
-  ),
-);
+    });
+  },
+}));
