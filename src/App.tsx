32d86c7fdb4c55c60f15dabda913e3e558db0d6f
Harshit Garg <gargh2120@gmail.com>
2026-02-27 20:46:07 +0530

    Initialize protected routes to improve security purpose

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,27 @@ import ProfileCard from "./Pages/Student/ProfileCard";
 import EducatorProfile from "./Pages/Educator/ProfileCard";
 import Footer from "./Componets/Student/Footer";
 import Addsections from "./Pages/Educator/Addsections";
+import Enrolled from "./Pages/Student/Enrolled";
+import ManageCourses from "./Pages/Educator/ManageCourse";
+import Analytics from "./Pages/Educator/Analytics";
+import ProtectedRoute from "./Componets/ProtectedRoute";
 
 function App() {
   const isEducatorPage = useMatch("/Educator/*");
+
   const getuser = useAuth((s) => s.CurrentUser);
+  const isBooting = useAuth((s) => s.isBooting);
+
   useEffect(() => {
     getuser();
   }, []);
+
+  if (isBooting) return <Loading />;
+
   return (
     <>
       {!isEducatorPage && <NavBar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -36,18 +47,65 @@ function App() {
         <Route path="/Course-List" element={<CoursesList />} />
         <Route path="/Course-List/:input" element={<CoursesList />} />
         <Route path="/Course-Details/:id" element={<CourseDetails />} />
-        <Route path="/My-Enroll" element={<MyEnrollment />} />
-        <Route path="/Player/:courseId" element={<Player />} />
-        <Route path="/Loading/:path" element={<Loading />} />
-        <Route path="/Profile" element={<ProfileCard />} />
-        <Route path="/Educator" element={<Educator />}>
+
+        {/*  Student Protected */}
+        <Route
+          path="/My-Enroll"
+          element={
+            <ProtectedRoute role="student">
+              <MyEnrollment />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/Player/:courseId"
+          element={
+            <ProtectedRoute role="student">
+              <Player />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/Profile"
+          element={
+            <ProtectedRoute>
+              <ProfileCard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/EnrollC"
+          element={
+            <ProtectedRoute role="student">
+              <Enrolled />
+            </ProtectedRoute>
+          }
+        />
+
+        {/*  Educator Protected Layout */}
+        <Route
+          path="/Educator"
+          element={
+            <ProtectedRoute role="educator">
+              <Educator />
+            </ProtectedRoute>
+          }
+        >
           <Route path="DashBoard" element={<DashBoard />} />
           <Route path="Profile" element={<EducatorProfile />} />
           <Route path="MyCourses" element={<MyCourses />} />
           <Route path="StudentsEnroll" element={<StudentEnrolled />} />
           <Route path="AddCourse" element={<AddCourse />} />
           <Route path="AddSection/:courseId" element={<Addsections />} />
+          <Route path="Manage-Course" element={<ManageCourses />} />
+          <Route path="Analytics" element={<Analytics />} />
         </Route>
+
+        {/* Optional Loading Route */}
+        <Route path="/Loading/:path" element={<Loading />} />
       </Routes>
 
       <Footer />
