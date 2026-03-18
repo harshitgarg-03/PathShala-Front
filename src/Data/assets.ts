531074ad4bd2514b3fbaf471c2d2db64db6b367c
Harshit Garg <gargh2120@gmail.com>
2026-03-18 22:43:04 +0530

    assests file

--- src/Data/assets.ts
+++ src/Data/assets.ts
@@ -42,7 +42,7 @@ import profile_img from './profile_img.png'
 import profile_img2 from './profile_img2.png'
 import profile_img3 from './profile_img3.png'
 import lesson_icon from './lesson_icon.svg'
-import type { dummyCoursesProp } from '../Types'
+// import type { dummyCoursesProp } from '../Types'
 
 
 export const assets = {
