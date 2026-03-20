d9c4bea999355ff44fccf596ce4f07f121722585
Harshit Garg <gargh2120@gmail.com>
2026-03-20 15:04:17 +0530

    Fix frontend issues

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "./", 
+  server: {
+    host: true,
+    port: 5173
+  }
 })
